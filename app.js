9fe70af4989e1c7ce72c190851b5ad20cb2ede43
Zak <zakattack26111@gmail.com>
2017-09-10 12:49:18 -1000

    added text bounce

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ function start() {
 	//the "[0]" is needed as jQuery returns the DOM element in an array
 	$('#info')[0].style.animationPlayState = "running";
 
-	setTimeout(() => { $('#welcomeScreen')[0].style.animationPlayState = "running";}, 550)
+	setTimeout(() => { $('#welcomeScreen')[0].style.animationPlayState = "running";}, 550);
 
   setTimeout(() => { 
 		$('#welcomeScreen')[0].style.display = "none";
 		$('#chat')[0].style.display = "inline-block";
 		$('#daizzy')[0].style.animationPlayState = "running";
-	}, 1750); //makes welcome screen invisible (display: none doe snot work within keyframes)
+	}, 1730); //makes welcome screen invisible (display: none doe snot work within keyframes)
 
+  setTimeout(() => {$('#chatTitle')[0].style.animationPlayState = "running";}, 2200);
 }
