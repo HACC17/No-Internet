e3b60ced66ddcadab081348db4f90b2bde64451a
Zak <zakattack26111@gmail.com>
2017-09-10 12:13:49 -1000

    redid animation

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 function start() {
 	//$('chat')[0] is equivalent to document.getElementById('chat')
 	//the "[0]" is needed as jQuery returns the DOM element in an array
-	$('#welcomeScreen')[0].style.animationPlayState = "running";
+	$('#info')[0].style.animationPlayState = "running";
 
-	setTimeout(() => { $('#chat')[0].style.animationPlayState = "running"; }, 450)
+	setTimeout(() => { $('#welcomeScreen')[0].style.animationPlayState = "running";}, 550)
 
   setTimeout(() => { 
 		$('#welcomeScreen')[0].style.display = "none";
+		$('#chat')[0].style.display = "inline-block";
 		$('#daizzy')[0].style.animationPlayState = "running";
-	}, 1400); //makes welcome screen invisible (display: none doe snot work within keyframes)
+	}, 1750); //makes welcome screen invisible (display: none doe snot work within keyframes)
 
 }
