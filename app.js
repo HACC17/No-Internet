29416e34c3ff3260b482308d472b8a7eb971de71
Zak <zakattack26111@gmail.com>
2017-09-10 00:09:36 -1000

    started HTML, added CSS animations

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Sanity Check");
+function start() {
+	//$('chat')[0] is equivalent to document.getElementById('chat')
+	//the "[0]" is needed as jQuery returns the DOM element in an array
+	$('#welcomeScreen')[0].style.animationPlayState = "running";
+
+	setTimeout(() => { $('#chat')[0].style.animationPlayState = "running"; }, 450)
+
+  setTimeout(() => { 
+		$('#welcomeScreen')[0].style.display = "none";
+		$('#daizzy')[0].style.animationPlayState = "running";
+	}, 1400); //makes welcome screen invisible (display: none doe snot work within keyframes)
+
+}
